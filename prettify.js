e72e53735bfe82ebd16d5806ebec149901e74007
jopinsky <53393266+jopinsky@users.noreply.github.com>
2020-01-13 20:51:30 +0800

    learning DOM, html walker

---
+++ prettify.js
@@ -0,0 +1,34 @@
+const treeWalker = (div) => {
+    if (div instanceof Text && div.parentNode.tagName === 'DIV') {
+      const pEl = document.createElement('p')
+      const textNode = document.createTextNode(div.textContent.trim())
+      if (textNode.length > 0) {
+        pEl.append(textNode)
+        div.replaceWith(pEl)
+      }
+    }
+      div.childNodes.forEach(treeWalker)
+  }
+  
+  const prettifier = (doc) => {
+    const divs = [...doc.getElementsByTagName('div')]
+    divs.forEach(treeWalker)
+  }
+  
+  ///// teacher's solution
+  //this solution is so clean holy macaroni
+  
+  export default (document) => {
+    const divs = document.getElementsByTagName('div');
+  
+    divs.forEach((div) => {
+      const textNodes = [...div.childNodes]
+        .filter((child) => child instanceof Text)
+        .filter((child) => child.textContent.trim() !== '');
+      textNodes.forEach((node) => {
+        const p = document.createElement('p');
+        p.textContent = node.textContent;
+        node.replaceWith(p);
+      });
+    });
+  };
